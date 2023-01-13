0377d80ad103c4a5f85244962d9491952f9b2f63
WesleySanchesBahia <57376670+WesleySanchesBahia@users.noreply.github.com>
2023-01-13 09:59:30 -0300

    desafio #boraCodar 02

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function clickVetor() {
+    vetor.style.display = "none"
+    imagem.style.display = "none"
+
+    imagem_animada.style.display = "flex"
+    vetor_close.style.display = "flex"
+}
+function clickVetorClose() {
+    imagem_animada.style.display = "none"
+    vetor_close.style.display = "none"
+
+    vetor.style.display = "flex"
+    imagem.style.display = "flex"
+}
